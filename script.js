b4e40fa58926e43f99781b9ccc630fc5ca3d0d83
Qiaogun <zbc0319@live.jp>
2020-11-29 03:37:21 +0900

    v1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function changewidth () {
+    var new_pad_block = document.getElementsByClassName("pad-block");
+    new_pad_block[0].style.marginLeft = 7 + "em";
+    new_pad_block[0].style.marginRight = 7 + "em";
+}
+changewidth()
