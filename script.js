e586804cb7012cb2d46540515106484ab2d2ebb6
Qiaogun <zbc0319@live.jp>
2020-11-29 20:36:30 +0900

    增加判断

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function changewidth () {
-    var new_pad_block = document.getElementsByClassName("pad-block");
-    new_pad_block[0].style.marginLeft = 7 + "em";
-    new_pad_block[0].style.marginRight = 7 + "em";
+function changewidth() {
+    if (document.documentElement.clientWidth > 1000) {
+        var new_pad_block = document.getElementsByClassName("pad-block");
+        new_pad_block[0].style.marginLeft = 7 + "em";
+        new_pad_block[0].style.marginRight = 7 + "em";
+    }
 }
 changewidth()
